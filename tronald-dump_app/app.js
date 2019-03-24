fc1903d7f954e09eb47c8bafba42c1971c493e90
dannypadilla <dpadilla010@gmail.com>
2019-03-23 18:18:31 -0700

    add id list

--- tronald-dump_app/app.js
+++ tronald-dump_app/app.js
@@ -1,5 +1,6 @@
 
-const superagent = require("superagent")
+//const superagent = require("superagent")
+const dump = require("tronald-dump")
 const inquirer = require("inquirer")
 
 async function get_quote(search = "", random = true, num = 1) {
@@ -7,25 +8,19 @@ async function get_quote(search = "", random = true, num = 1) {
   if (search != "") {
     random = false
   }
-  const api_base = "https://api.tronalddump.io";
-  const search_base = "search/quote?query=";
-  const fetch_base = "quote"
 
-  const default_search = "obama"
+  const query = await dump.search(search);
+  //const quotes = await dump.get_quotes(query);
+  let ids = await dump.get_id_list(query);
+  //console.log(quotes);
+  console.log(ids);
+  //console.log("\nThere are a total of", count, "quotes\n")
 
-  const search_response = await superagent.get(`${api_base}/${!random ? search_base + search: search_base + default_search}`)
-
-  let count = search_response.body.total
-  console.log("\nThere are a total of", count, "quotes\n")
-  //console.log(search_response.body)
-  //console.log(search_response.body._embedded.quotes)
   let prompt_quote = await another_quote();
   //console.log(prompt_quote);
 
   while (prompt_quote.quote) {
-
     console.log("\nok\n")
-
     prompt_quote = await another_quote();
   }
 
